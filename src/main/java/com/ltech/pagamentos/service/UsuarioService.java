0d0df3ffa58a7bda95fca1fd2eff787c72d725f4
conuaz <lucas.olasz@tty2000.com.br>
2025-02-17 16:10:42 -0300

    Limpeza de importações

--- src/main/java/com/ltech/pagamentos/service/UsuarioService.java
+++ src/main/java/com/ltech/pagamentos/service/UsuarioService.java
@@ -2,8 +2,6 @@ package com.ltech.pagamentos.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
