7bb3ff8acca08ff470c186a528f68dc14561f176
conuaz <lucas.olasz@tty2000.com.br>
2025-02-20 15:58:18 -0300

    Inclui o jquery ui e mask

--- src/main/resources/static/js/custom-script.js
+++ src/main/resources/static/js/custom-script.js
@@ -2,4 +2,21 @@
 $(document).ready(function () {
     // Exibe o Toast Mensagem automaticamente
     $('#toast').toast('show');
+
+    // Ativar automaticamente o data-mask
+    $('[data-mask]').each(function () {
+        $(this).mask($(this).attr('data-mask'));
+    });
+
+    // Aplicar máscara e Datepicker a todos os campos com a classe .data-picker
+    $(".datePicker").each(function () {
+        $(this).datepicker({
+            dateFormat: "dd/mm/yy",
+            changeMonth: true,
+            changeYear: true,
+            yearRange: "1900:2100",
+            regional: "pt-BR"
+        });
+    });
+
 });
