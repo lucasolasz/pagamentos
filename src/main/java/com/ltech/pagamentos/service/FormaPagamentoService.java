8dc789438883281fb76c0e0b1e955253ff4adc29
conuaz <lucas.olasz@tty2000.com.br>
2025-02-27 17:56:02 -0300

    Correção de anotação

--- src/main/java/com/ltech/pagamentos/service/FormaPagamentoService.java
+++ src/main/java/com/ltech/pagamentos/service/FormaPagamentoService.java
@@ -1,9 +1,12 @@
 package com.ltech.pagamentos.service;
 
+import org.springframework.stereotype.Service;
+
 import com.ltech.pagamentos.model.FormaPagamento;
 import com.ltech.pagamentos.padrao.ServiceCrud;
 import com.ltech.pagamentos.repository.FormaPagamentoRepository;
 
+@Service
 public class FormaPagamentoService extends ServiceCrud<FormaPagamento, Long, FormaPagamentoRepository> {
 
     public FormaPagamentoService(FormaPagamentoRepository repository) {
--- src/main/java/com/ltech/pagamentos/service/TipoUsuarioService.java
+++ src/main/java/com/ltech/pagamentos/service/TipoUsuarioService.java
@@ -1,9 +1,12 @@
 package com.ltech.pagamentos.service;
 
+import org.springframework.stereotype.Service;
+
 import com.ltech.pagamentos.model.TipoUsuario;
 import com.ltech.pagamentos.padrao.ServiceCrud;
 import com.ltech.pagamentos.repository.TipoUsuarioRepository;
 
+@Service
 public class TipoUsuarioService extends ServiceCrud<TipoUsuario, Long, TipoUsuarioRepository> {
 
     public TipoUsuarioService(TipoUsuarioRepository repository) {
